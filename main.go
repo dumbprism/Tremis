7d5260079b64d82f54b2a5bd053b0b6dbccdb2ef
Dumbprism <arhantk915@gmail.com>
2024-10-12 04:56:05 +0530

    addded PUB/SUB command

--- main.go
+++ main.go
@@ -4,18 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 type store struct {
-	data map[string]string 
+	data map[string]string
 	list map[string][]string
 	sets map[string]map[string]bool
-
+	subs map[string][]client
 }
-
-
 
 func (s *store) set(key string, value string) string {
 	s.data[key] = value
@@ -39,62 +37,67 @@ func (s *store) del(key string) string {
 	return "NULL" // Returning NULL if key doesn't exist
 }
 
-func (s *store) handleCommand(command string, args []string) string {
-    switch command {
+func (s *store) handleCommand(command string, args []string, conn net.Conn) string {
+	switch command {
 
-    case "SET":
-        // Using Join to save the rest of the received data
-        return s.set(args[0], strings.Join(args[1:], " "))
-		
-    case "GET":
-        return s.get(args[0])
-		
-    case "DEL":
-        s.del(args[0])
-        return "DELETED"
-	
-    case "INCR":
-        return fmt.Sprintf("%v", s.incr(args[0]))
-    case "INCRBY":
-        return fmt.Sprintf("%v", s.incrBy(args[0], args[1]))
-    case "DECR":
-        return fmt.Sprintf("%v", s.decr(args[0]))
-    case "DECRBY":
-        return fmt.Sprintf("%v", s.decrBy(args[0], args[1]))
+	case "SET":
+		// Using Join to save the rest of the received data
+		return s.set(args[0], strings.Join(args[1:], " "))
+
+	case "GET":
+		return s.get(args[0])
+
+	case "DEL":
+		s.del(args[0])
+		return "DELETED"
+
+	case "INCR":
+		return fmt.Sprintf("%v", s.incr(args[0]))
+	case "INCRBY":
+		return fmt.Sprintf("%v", s.incrBy(args[0], args[1]))
+	case "DECR":
+		return fmt.Sprintf("%v", s.decr(args[0]))
+	case "DECRBY":
+		return fmt.Sprintf("%v", s.decrBy(args[0], args[1]))
 	case "LPUSH":
-        return fmt.Sprintf("%v", s.lPush(args[0], args[1]))
-   case "RPUSH":
-        return fmt.Sprintf("%v", s.rPush(args[0], args[1]))
-   case "LPOP":
-        return s.lPop(args[0])
-   case "RPOP":
-        return s.rPop(args[0])
-   case "LLEN":
-        return fmt.Sprintf("%v", s.lLen(args[0]))
-    case "LINDEX":
-        index, _ := strconv.Atoi(args[1])
-        return s.lIndex(args[0], index)
+		return fmt.Sprintf("%v", s.lPush(args[0], args[1]))
+	case "RPUSH":
+		return fmt.Sprintf("%v", s.rPush(args[0], args[1]))
+	case "LPOP":
+		return s.lPop(args[0])
+	case "RPOP":
+		return s.rPop(args[0])
+	case "LLEN":
+		return fmt.Sprintf("%v", s.lLen(args[0]))
+	case "LINDEX":
+		index, _ := strconv.Atoi(args[1])
+		return s.lIndex(args[0], index)
 	case "SADD":
-        return fmt.Sprintf("%v", s.sadd(args[0], args[1]))
-     case "SREM":
-        return fmt.Sprintf("%v", s.srem(args[0], args[1]))
-     case "SMEMBERS":
-        members := s.smembers(args[0])
-        result := ""
-      
-        for _, member := range members {
-           result += fmt.Sprintf("%v ", member)
-        }
-      
-        return strings.TrimSpace(result)
-     case "SISMEMBER":
-        return fmt.Sprintf("%v", s.sismember(args[0], args[1]))
-	
-	default:
-        return "ERROR : unkown command"
-    }
-}
+		return fmt.Sprintf("%v", s.sadd(args[0], args[1]))
+	case "SREM":
+		return fmt.Sprintf("%v", s.srem(args[0], args[1]))
+	case "SMEMBERS":
+		members := s.smembers(args[0])
+		result := ""
 
+		for _, member := range members {
+			result += fmt.Sprintf("%v ", member)
+		}
+
+		return strings.TrimSpace(result)
+	case "SISMEMBER":
+		return fmt.Sprintf("%v", s.sismember(args[0], args[1]))
+
+	case "SUBSCRIBE":
+		return s.subscribe(args[0], conn)
+	case "PUBLISH":
+		s.publish(args[0], strings.Join(args[1:], " "))
+		return "OK"
+
+	default:
+		return "ERROR : unkown command"
+	}
+}
 
 func handleConnection(conn net.Conn, s *store) {
 	defer conn.Close()
@@ -113,7 +116,7 @@ func handleConnection(conn net.Conn, s *store) {
 		command := strings.ToUpper(parts[0]) // Make command case-insensitive
 		args := parts[1:]
 
-		response := s.handleCommand(command, args)
+		response := s.handleCommand(command, args, conn)
 
 		fmt.Fprintln(conn, response) // Respond to client
 	}
@@ -125,8 +128,7 @@ func main() {
 		data: make(map[string]string),
 		list: make(map[string][]string),
 		sets: make(map[string]map[string]bool),
-
-		 
+		subs: make(map[string][]client),
 	}
 
 	listener, err := net.Listen("tcp", ":6379")
@@ -147,4 +149,3 @@ func main() {
 		go handleConnection(conn, s)
 	}
 }
-
